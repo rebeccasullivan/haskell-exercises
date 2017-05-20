3c2037079cd85e35353f0c076a7dc824d12d636e
Rebecca <rhhsullivan@gmail.com>
2017-05-20 09:14:45 -0400

    Refactored with hlint suggestions

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -1,12 +1,7 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = if (year `mod` 400) == 0
-                  then True
-                  else 
-                    if (year `mod` 4 == 0) && not (year `mod` 100 == 0)
-                    then True
-                    else False
+isLeapYear year =  year `mod` 400 == 0 ||(year `mod` 4 == 0 && year `mod` 100 /= 0)
 
 -- on every year that is evenly divisible by 4
 --  except every year that is evenly divisible by 100
